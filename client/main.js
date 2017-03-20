97b44b6b97359d73e87af8da1d806d0173b67a9e
AdarshGupta <adarsh035@gmail.com>
2017-03-20 18:58:53 +0530

    Decorated temporary Abstract

--- client/main.js
+++ client/main.js
@@ -36,5 +36,5 @@ Accounts.ui.config({
   requestOfflineToken: {
     // google: true
   },
-  passwordSignupFields: 'USERNAME_ONLY' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
+  passwordSignupFields: 'USERNAME_AND_EMAIL' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
 });
